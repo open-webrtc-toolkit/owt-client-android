0e4d080f219b43cfa3b5f5db3ffae870524e8f56
Zhang He <he.zhang@intel.com>
2018-05-29 09:06:37 +0800

    Fixed nullptr issue on ConferenceClient.conferenceInfo
    ConferenceClient.conferenceInfo was previously accessed and modified on
    two difference threads, which caused this issue.
    
    Tracked-On: https://jira01.devtools.intel.com/browse/ICFWX-1533
    Change-Id: Icd26b4e56d9baed5aa2000df168b2d161b839e9e
    Reviewed-on: https://git-ccr-1.devtools.intel.com/gerrit/86426
    Reviewed-by: Qiu, Jianlin <jianlin.qiu@intel.com>
    Tested-by: Qiu, Jianlin <jianlin.qiu@intel.com>

--- src/sdk/conference/src/main/java/com/intel/webrtc/conference/ConferenceClient.java
+++ src/sdk/conference/src/main/java/com/intel/webrtc/conference/ConferenceClient.java
@@ -646,10 +646,10 @@ public final class ConferenceClient implements SignalingChannel.SignalingChannel
     public void onRoomDisconnected() {
         DCHECK(callbackExecutor);
         changeRoomStatus(RoomStates.DISCONNECTED);
-        closeInternal();
         callbackExecutor.execute(new Runnable() {
             @Override
             public void run() {
+                closeInternal();
                 for (ConferenceClientObserver observer : observers) {
                     observer.onServerDisconnected();
                 }
