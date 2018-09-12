96be6c9aa0784deb68fb9dfefea9ed156cffab4d
Zhang He <he.zhang@intel.com>
2018-09-12 10:47:25 +0800

    Handle reconnection in p2p sample.
    And disable the reconnection feature in test codes.
    
    Change-Id: I314a4ecad914083c4dd15bea50db2e7ccc9f1ecc
    Reviewed-on: https://git-ccr-1.devtools.intel.com/gerrit/99279
    Tested-by: webrtctest <webrtctest@intel.com>
    Reviewed-by: Zhang, He <he.zhang@intel.com>

--- src/sample/p2p/src/main/java/com/intel/webrtc/p2p/sample/SocketSignalingChannel.java
+++ src/sample/p2p/src/main/java/com/intel/webrtc/p2p/sample/SocketSignalingChannel.java
@@ -66,87 +66,79 @@ public class SocketSignalingChannel implements SignalingChannelInterface {
     private final String CLIENT_VERSION = "&clientVersion=";
     private final String CLIENT_VERSION_VALUE = IcsConst.CLIENT_VERSION;
 
+    private final int MAX_RECONNECT_ATTEMPTS = 5;
+    private int reconnectAttempts = 0;
     private Socket socketIOClient;
-    private List<SignalingChannelInterface.SignalingChannelObserver> signalingChannelObservers;
+    private List<SignalingChannelObserver> signalingChannelObservers;
     private ActionCallback<String> connectCallback;
 
-
-    private Listener onServerAuthenticatedCallback = new Listener() {
-        @Override
-        public void call(Object... arg0) {
-            if (connectCallback != null) {
-                connectCallback.onSuccess(arg0[0].toString());
-            }
-        }
-    };
-
-    private Listener onConnectFailedCallback = new Listener() {
-        @Override
-        public void call(Object... arg0) {
+    // Socket.IO events.
+    private Listener onConnectErrorCallback = args -> {
+        if (reconnectAttempts >= MAX_RECONNECT_ATTEMPTS) {
             if (connectCallback != null) {
                 connectCallback.onFailure(
                         new IcsError(P2P_CONN_SERVER_UNKNOWN.value, "connect failed"));
-            }
-        }
-    };
-
-    private Listener onForceDisconnectCallback = new Listener() {
-        @Override
-        public void call(Object... arg0) {
-            if (socketIOClient != null) {
-                socketIOClient.io().reconnection(false);
-            }
-        }
-    };
-
-    private Listener onDisconnectCallback = new Listener() {
-        @Override
-        public void call(Object... arg0) {
-            for (SignalingChannelObserver observer : signalingChannelObservers) {
-                observer.onServerDisconnected();
-            }
-        }
-    };
-
-    private Listener onServerErrorCallback = new Listener() {
-
-        @Override
-        public void call(Object... arg0) {
-            if (connectCallback != null) {
-                Pattern pattern = Pattern.compile("[0-9]*");
-                if (pattern.matcher(arg0[0].toString()).matches()) {
-                    connectCallback.onFailure(
-                            new IcsError(IcsP2PError.get(Integer.parseInt((String) arg0[0])).value,
-                                    "Server error"));
-                } else {
-                    connectCallback.onFailure(new IcsError(arg0[0].toString()));
+                connectCallback = null;
+            } else {
+                for (SignalingChannelObserver observer : signalingChannelObservers) {
+                    observer.onServerDisconnected();
                 }
             }
+            reconnectAttempts = 0;
         }
-
     };
 
-    private Listener onMessageCallback = new Listener() {
-        @Override
-        public void call(Object... arg0) {
-            JSONObject argumentJsonObject = (JSONObject) arg0[0];
-            for (SignalingChannelObserver observer : signalingChannelObservers) {
-                try {
-                    observer.onMessage(argumentJsonObject.getString("from"),
-                            argumentJsonObject.getString("data"));
-                } catch (JSONException e) {
-                    e.printStackTrace();
-                }
+    private Listener onErrorCallback = args -> {
+        if (connectCallback != null) {
+            Pattern pattern = Pattern.compile("[0-9]*");
+            if (pattern.matcher(args[0].toString()).matches()) {
+                connectCallback.onFailure(
+                        new IcsError(IcsP2PError.get(Integer.parseInt((String) args[0])).value,
+                                "Server error"));
+            } else {
+                connectCallback.onFailure(new IcsError(args[0].toString()));
             }
         }
     };
 
-    /**
-     * Initialize the socket client.
-     */
+    private Listener onReconnectingCallback = args -> {
+        reconnectAttempts++;
+    };
+
+    private Listener onDisconnectCallback = args -> {
+        for (SignalingChannelObserver observer : signalingChannelObservers) {
+            observer.onServerDisconnected();
+        }
+    };
+
+    // P2P server events.
+    private Listener onServerAuthenticatedCallback = args -> {
+        if (connectCallback != null) {
+            connectCallback.onSuccess(args[0].toString());
+            connectCallback = null;
+        }
+    };
+
+    private Listener onForceDisconnectCallback = args -> {
+        if (socketIOClient != null) {
+            socketIOClient.on(Socket.EVENT_DISCONNECT, onDisconnectCallback);
+            socketIOClient.io().reconnection(false);
+        }
+    };
+
+    private Listener onMessageCallback = args -> {
+        JSONObject argumentJsonObject = (JSONObject) args[0];
+        for (SignalingChannelObserver observer : signalingChannelObservers) {
+            try {
+                observer.onMessage(argumentJsonObject.getString("from"),
+                        argumentJsonObject.getString("data"));
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+    };
+
     SocketSignalingChannel() {
-        socketIOClient = null;
-        connectCallback = null;
         this.signalingChannelObservers = new ArrayList<>();
     }
 
@@ -170,8 +162,8 @@ public class SocketSignalingChannel implements SignalingChannelInterface {
             loginObject = new JSONObject(userInfo);
             token = URLEncoder.encode(loginObject.getString("token"), "UTF-8");
             url = loginObject.getString("host");
-            url += "?token=" + token + CLIENT_TYPE + CLIENT_TYPE_VALUE
-                    + CLIENT_VERSION + CLIENT_VERSION_VALUE;
+            url += "?token=" + token + CLIENT_TYPE + CLIENT_TYPE_VALUE + CLIENT_VERSION
+                    + CLIENT_VERSION_VALUE;
             if (!isValid(url)) {
                 callback.onFailure(new IcsError(P2P_CLIENT_ILLEGAL_ARGUMENT.value, "Invalid URL"));
                 return;
@@ -179,12 +171,20 @@ public class SocketSignalingChannel implements SignalingChannelInterface {
             IO.Options opt = new IO.Options();
             opt.forceNew = true;
             opt.reconnection = true;
+            opt.reconnectionAttempts = MAX_RECONNECT_ATTEMPTS;
             if (socketIOClient != null) {
                 Log.d(TAG, "stop reconnecting the former url");
                 socketIOClient.disconnect();
             }
             socketIOClient = IO.socket(url, opt);
-            bindCallbacks();
+
+            socketIOClient.on(Socket.EVENT_CONNECT_ERROR, onConnectErrorCallback)
+                    .on(Socket.EVENT_ERROR, onErrorCallback)
+                    .on(Socket.EVENT_RECONNECTING, onReconnectingCallback)
+                    .on(CLIENT_CHAT_TYPE, onMessageCallback)
+                    .on(SERVER_AUTHENTICATED, onServerAuthenticatedCallback)
+                    .on(FORCE_DISCONNECT, onForceDisconnectCallback);
+
             socketIOClient.connect();
 
         } catch (JSONException e) {
@@ -211,22 +211,11 @@ public class SocketSignalingChannel implements SignalingChannelInterface {
         }
     }
 
-    /**
-     * Bind callback to the socket IO client.
-     */
-    private void bindCallbacks() {
-        socketIOClient.on(CLIENT_CHAT_TYPE, onMessageCallback)
-                .on(SERVER_AUTHENTICATED, onServerAuthenticatedCallback)
-                .on(FORCE_DISCONNECT, onForceDisconnectCallback)
-                .on(Socket.EVENT_CONNECT_ERROR, onConnectFailedCallback)
-                .on(Socket.EVENT_DISCONNECT, onDisconnectCallback)
-                .on(Socket.EVENT_ERROR, onServerErrorCallback);
-    }
-
     @Override
     public void disconnect() {
         if (socketIOClient != null) {
             Log.d(TAG, "Socket IO Disconnect.");
+            socketIOClient.on(Socket.EVENT_DISCONNECT, onDisconnectCallback);
             socketIOClient.disconnect();
             socketIOClient = null;
         }
