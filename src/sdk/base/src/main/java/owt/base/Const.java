2831accddc27325c9398dcdec247074cfd7b8710
taste1981 <jianlin.qiu@intel.com>
2020-11-20 10:04:17 +0800

    Do not set max bitrate when invalid value specified. (#224)

--- src/sdk/base/src/main/java/owt/base/Const.java
+++ src/sdk/base/src/main/java/owt/base/Const.java
@@ -11,7 +11,7 @@ public class Const {
     public static final String LOG_TAG = "OWT";
     public static final String CLIENT_VERSION = "4.3";
     public static final String userAgent = "{" +
-            "'runtime':{'name':'webrtc','version':'70'}," +
+            "'runtime':{'name':'webrtc','version':'83'}," +
             "'sdk':{'type':'Android','version':" + CLIENT_VERSION + "}," +
             "'os':{'name':'Android', 'version':" + Build.VERSION.RELEASE + "}," +
             "'capabilities':{'continualIceGathering': true," +
